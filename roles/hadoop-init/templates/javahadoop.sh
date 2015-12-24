44494a5effb4c4e2941100febbfafd2b86b16f3f
kingoliang <kingo.liang@gmail.com>
2015-12-24 15:32:01 +0800

    Add Hadoop Install

---
+++ roles/hadoop-init/templates/javahadoop.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:/opt/hadoop/lib/native/:$JAVA_HOME/jre/lib/amd64/server/
+export CLASSPATH=$(/opt/hadoop/bin/hadoop classpath | sed s/:/'\n'/g | awk '/\*$/ {print "ls", $0 ".jar"}' | sh | sed ':a;N;$!ba;s/\n/:/g')
+export PATH=$PATH:/opt/hadoop/bin:/opt/hadoop/sbin
