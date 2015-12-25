f84b87665ea28c01cf732971a91ec549715ee1cc
kingoliang <kingo.liang@gmail.com>
2015-12-25 17:02:23 +0800

    Modify Config

--- roles/hadoop-init/templates/javahadoop.sh
+++ roles/hadoop-init/templates/javahadoop.sh
@@ -1,3 +1,5 @@
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:/opt/hadoop/lib/native/:$JAVA_HOME/jre/lib/amd64/server/
-export CLASSPATH=$(/opt/hadoop/bin/hadoop classpath | sed s/:/'\n'/g | awk '/\*$/ {print "ls", $0 ".jar"}' | sh | sed ':a;N;$!ba;s/\n/:/g')
-export PATH=$PATH:/opt/hadoop/bin:/opt/hadoop/sbin
+if [ `whoami` = "{{ hadoop_user }}" ]; then
+  export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server:/opt/hadoop/lib/native/:$JAVA_HOME/jre/lib/amd64/server/
+  export CLASSPATH=$(/opt/hadoop/bin/hadoop classpath | sed s/:/'\n'/g | awk '/\*$/ {print "ls", $0 ".jar"}' | sh | sed ':a;N;$!ba;s/\n/:/g')
+  export PATH=$PATH:/opt/hadoop/bin:/opt/hadoop/sbin
+fi
